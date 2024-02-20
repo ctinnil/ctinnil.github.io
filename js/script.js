481e7842ca936e3ec7c681a5c1181c08c75451c1
ctinnil <69745175+ctinnil@users.noreply.github.com>
2024-02-20 16:14:34 +0000

    main

--- js/script.js
+++ js/script.js
@@ -1,20 +1,13 @@
-const hamburger = document.getElementsByClassName("hamburger")[0]
-const mobileNavs = document.getElementsByClassName("nav-links")[0]
-
-hamburger.addEventListener("click", () => {
-  mobileNavs.classList.toggle("active")
-})
-
 document.addEventListener('DOMContentLoaded', function() {
-    var titles = document.querySelectorAll('.entry .title');
-  
-    titles.forEach(function(title) {
-      title.addEventListener('mouseover', function() {
-        this.classList.add('big');
-      });
-      
-      title.addEventListener('mouseout', function() {
-        this.classList.remove('big');
-      });
+  var titles = document.querySelectorAll('.entry .title');
+
+  titles.forEach(function(title) {
+    title.addEventListener('mouseover', function() {
+      this.classList.add('big');
+    });
+    
+    title.addEventListener('mouseout', function() {
+      this.classList.remove('big');
     });
   });
+});
